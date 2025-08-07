f22d3925c6724cf39114c21f3850fa4091fde013
AdiQxD <adiqxd@gmail.com>
2025-08-07 08:43:49 +0000

    Added input for main menu

---
+++ main.py
@@ -0,0 +1,7 @@
+input("Menadzer finansów CLI\n" \
+    "1. Dodaj transakcję\n" \
+    "2. Historia transakcji\n" \
+    "3. Analiza salda\n" \
+    "4. Eksportuj dane\n" \
+    "5. Wyjście\n\n" \
+    "Twój wybór: ")
